1c72a5b066b61463f4083d2cf6dc203abe1ae769
sdarna94 <156135979+sdarna94@users.noreply.github.com>
2025-12-11 14:38:32 -0500

    Basic

---
+++ data-type.go
@@ -0,0 +1,29 @@
+package main
+
+//import "fmt"
+
+var age int = 25 // package level variable
+// count := 10  // invalid syntax for package level variable
+
+func main() {
+	//--------data types in golang--------
+
+	//var declaration
+
+	var age int = 30 // new changes
+	var name = "John Doe"
+
+	//short hand declaration
+
+	count := 10
+	name1 := "Alice"
+
+	//default values
+	// Numaric types default to 0
+	// String defaults to ""
+	// Boolean defaults to false
+	//pointer , slice , map , function , interface  default to nil
+
+	//--------scope of variable--------
+
+}
