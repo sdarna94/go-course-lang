76285d0538822596fe3b031f299a18342982f6d7
sdarna94 <chaitanyadarna94@gmail.com>
2025-12-20 13:22:25 -0500

    new

---
+++ star patten/increcese1.java
@@ -0,0 +1,39 @@
+class increcese1
+{
+    public static void main(String[] args)
+    {
+       int n=5;
+       for (int i=1;i<=n;i++){
+        for (int j=1;j<=i;j++){
+            System.out.print("*");
+
+        }
+        System.out.println();
+
+       }
+       System.out.println("-------------------");
+       System.out.println("-------------------");
+       System.out.println("-------------------");
+
+       int r=5;
+       for (int i=1;i<=r;i++){
+        for (int j=i;j<=r;j++){
+            System.out.print("*");
+
+        }
+        System.out.println();
+
+       }
+           System.out.println("-------------------");
+       System.out.println("-------------------");
+       System.out.println("-------------------");
+
+       int q=5;
+       for (int i=1;i<=q;i++){
+        for (int j=i;j<=q;j++){
+            System.out.print("*");
+
+        }
+
+    }
+}
