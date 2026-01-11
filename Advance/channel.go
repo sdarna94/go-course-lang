8b22c069a8b2cb44421aea975b2ba31ecff42803
sdarna94 <chaitanyadarna94@gmail.com>
2026-01-11 11:11:27 -0600

    new

---
+++ Advance/channel.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	//variable := make([]type, size)
+	greeting := make(chan string) // create a channel
+
+	greetingsting := "Hello, World!"
+
+	go func() { // start a goroutine to send data to the channel
+		greeting <- greetingsting // send a value to the channel
+		greeting <- "this is golang channel example"
+		for _, e := range "Golang" {
+			greeting <- ("alphabeted are " + string(e))
+		}
+
+	}()
+
+	reciver := <-greeting // receive a value from the channel
+	fmt.Println(reciver)
+	reciver = <-greeting // receive a value from the channel
+	fmt.Println(reciver)
+
+	for range 6 {
+		reci := <-greeting
+		fmt.Println(reci)
+	}
+
+	time.Sleep(1 * time.Microsecond)
+	//fmt.Println(reciver)
+}
---
+++ Advance/go.mod
@@ -0,0 +1,3 @@
+module go-course-lang
+
+go 1.25.5
