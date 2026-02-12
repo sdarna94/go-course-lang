b35997c55d46f86ff3c0b71db74fac1d5ea799bf
sdarna94 <chaitanyadarna94@gmail.com>
2026-02-12 12:00:41 -0500

    commit

--- RestApIProject/server.go
+++ RestApIProject/server.go
@@ -7,9 +7,16 @@ import (
 )
 
 func main() {
+	// Define the handler for the /Order endpoint
+	http.HandleFunc("/c", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Order endpoint called")
+	})
 
 	port := 3030
 	fmt.Printf("Starting server on port %d\n", port)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil
+	if err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+	}
 
 }
---
+++ star patten/multiply.java
@@ -0,0 +1,27 @@
+// User function Template for Java
+import java.util.*; 
+
+class multiply {
+    
+    public ArrayList<Integer> getTable(int n) {
+        ArrayList<Integer> res = new ArrayList<>(10);
+        for (int i = 1; i <= 10; i++) {
+            res.add(n * i);    // n*1 ... n*10
+        }
+        return res;
+    }
+            
+         public static void main(String[] args) {
+             
+       Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        
+        multiply ob = new multiply();
+        ArrayList<Integer> ans = ob.getTable(n);   // ArrayList version
+
+        for (int x : ans) {
+            System.out.print(x + " ");
+        }
+    }
+}
