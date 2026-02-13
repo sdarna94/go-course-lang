70dedcc886ddd5a934cb570e47160984b695c217
sdarna94 <chaitanyadarna94@gmail.com>
2026-02-13 12:18:16 -0500

    commit

--- star patten/multiply.java
+++ star patten/multiply.java
@@ -1,27 +1,14 @@
 // User function Template for Java
 import java.util.*; 
 
-class multiply {
-    
-    public ArrayList<Integer> getTable(int n) {
-        ArrayList<Integer> res = new ArrayList<>(10);
-        for (int i = 1; i <= 10; i++) {
-            res.add(n * i);    // n*1 ... n*10 
-        }
-        return res;
-    }
-            
-         public static void main(String[] args) {
-             
-       Scanner sc = new Scanner(System.in);
+class Multiply {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.close();
-        
-        multiply ob = new multiply();
-        ArrayList<Integer> ans = ob.getTable(n);   // ArrayList version
 
-        for (int x : ans) {
-            System.out.print(x + " ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(n * i + " ");
         }
     }
 }
